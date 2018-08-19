c482688d9fe5de423095230776c6a269cb3b961c
Leviathan143 <Leviathan143@users.noreply.github.com>
2018-08-20 07:52:18 +1200

    Finish compileTransformers and transformersJar tasks

---
+++ src/reobfuscator/java/daomephsta/forgroovy/reobfuscator/DynamicCallReobfuscatingTransformer.java
@@ -0,0 +1,17 @@
+package daomephsta.forgroovy.reobfuscator;
+
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.transform.ASTTransformation;
+
+public class DynamicCallReobfuscatingTransformer implements ASTTransformation
+{
+
+	@Override
+	public void visit(ASTNode[] nodes, SourceUnit source)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
